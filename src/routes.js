94c1d80c91438bd269324f12709f03112730a5f6
Artem Dervoed <artem.dervoed@saltpepper.ru>
2019-02-09 21:12:53 +0300

    Remove pages 404 and users

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,5 @@
 import HomePage from '_pages/HomePage';
-import UserInfoPage from '_pages/UserInfoPage';
-import NotFoundPage from '_pages/NotFoundPage';
 import App from './app';
-
-import { fetchUsers } from '_sagas/users';
 
 export default [
   {
@@ -13,16 +9,6 @@ export default [
         path: '/',
         exact: true,
         component: HomePage,
-        sagasToRun: [
-          fetchUsers,
-        ],
-      },
-      {
-        path: '/users/:id',
-        component: UserInfoPage,
-      },
-      {
-        component: NotFoundPage,
       },
     ],
   },
